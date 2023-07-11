7fd539eea806eb8ed6dd404662ebac121b55a3db
kyanysh <kyanyshbd30@gmail.com>
2023-07-11 16:38:26 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const RegistrationForm = () => {
+        const [firstName, setFirstName] = useState('');
+        const [lastName, setLastName] = useState('');
+        const [email, setEmail] = useState('');
+        const [showData, setShowData] = useState(false);
 
-export default App;
+        const handleFirstNameChange = (event) => {
+            setFirstName(event.target.value);
+        };
+
+        const handleLastNameChange = (event) => {
+            setLastName(event.target.value);
+        };
+
+        const handleEmailChange = (event) => {
+            setEmail(event.target.value);
+        };
+
+        const handleSubmit = (event) => {
+            event.preventDefault();
+            setShowData(true);
+        };
+
+        //     return ( <
+        //         div style = {
+        //             { display: 'flex', flexDirection: 'column', alignItems: 'center' } } >
+        //         <
+        //         h2 > Registration Form < /h2> <
+        //         form onSubmit = { handleSubmit } >
+        //         <
+        //         input type = "text"
+        //         placeholder = "First Name"
+        //         value = { firstName }
+        //         onChange = { handleFirstNameChange }
+        //         style = {
+        //             { margin: '5px', padding: '5px', width: '200px', textAlign: 'center' } }
+        //         /> <
+        //         input type = "text"
+        //         placeholder = "Last Name"
+        //         value = { lastName }
+        //         onChange = { handleLastNameChange }
+        //         style = {
+        //             { margin: '5px', padding: '5px', width: '200px', textAlign: 'center' } }
+        //         /> <
+        //         input type = "email"
+        //         placeholder = "Email"
+        //         value = { email }
+        //         onChange = { handleEmailChange }
+        //         style = {
+        //             { margin: '5px', padding: '5px', width: '200px', textAlign: 'center' } }
+        //         /> <
+        //         button type = "submit"
+        //         style = {
+        //             { margin: '5px', padding: '5px', backgroundColor: 'green', color: 'white', border: 'none' } } >
+        //         Register <
+        //         /button> <
+        //         /form> {
+        //             showData && ( <
+        //                 div style = {
+        //                     { marginTop: '10px', textAlign: 'center' } } >
+        //                 <
+        //                 p > First Name: { firstName } < /p> <
+        //                 p > Last Name: { lastName } < /p> <
+        //                 p > Email: { email } < /p> <
+        //                 /div>
+        //             )
+        //         } <
+        //         /div>
+        //     );
+        // }
+
+        export default RegistrationForm;
